b0a711c7a57ffe53edf0a49939b2d5b629602231
Aaron France <aaron.l.france@gmail.com>
2014-02-19 10:40:09 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject crc16 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Creates CRC16s of byte[]s."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
